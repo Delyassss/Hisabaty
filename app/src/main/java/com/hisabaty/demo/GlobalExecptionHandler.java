ce6d5cf8efe3a1cb5e353632152988f0086deeb0
Ilyass Dabounou <89709305+Delyassss@users.noreply.github.com>
2026-09-26 20:30:56 +0000

    feat: add GlobalExceptionHandler to handle application exceptions

--- app/src/main/java/com/hisabaty/demo/GlobalExecptionHandler.java
+++ app/src/main/java/com/hisabaty/demo/GlobalExecptionHandler.java
@@ -4,15 +4,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
 @RestControllerAdvice
-@ServiceA
 public class GlobalExceptionHandler
 {
-    @ExecptionHandler(value = "illegalArgumentExeption")
+    @ExecptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<HashMap<String, String>> handleInvalidArgumentException(IllegalArgumentException ex)
     {
-        return  Map.of("error", ex.getMessage());
+        return  ResponseEntity.body(Map.of("error", ex.getMessage()));
     }
+
+    @ExecptionHandler(SchoolNotFound.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<HashMap<String, String>> handleSchoolNotFopund(SchoolNotFound  ex)
+    {
+        return  ResponseEntity.body(Map.of("error", ex.getMessage()));
+    }
+
+    @ExecptionHandler(ValueAlreadyExist.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<HashMap<String,String>> handleValueAlreadyExist(ValueAlreadyExist ex)
+    {
+        return ResponseEntity.body(Map.of("error", ex.getMessage()));
+    }
+
     
+
     
 }
